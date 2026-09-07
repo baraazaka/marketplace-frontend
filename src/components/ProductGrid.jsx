6958b1c26c1a77073407f882c951fb0d84e96834
baraazaka <bara.zaka@outlook.com>
2026-09-07 15:12:22 +0300

    add commponents productGrid

---
+++ src/components/ProductGrid.jsx
@@ -0,0 +1,27 @@
+
+import ProductCard from "./ProductCard";
+
+
+function ProductGrid({ products }) {
+
+    return (
+
+        <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+
+            {products.map((product) => (
+
+                <ProductCard
+                    key={product.id}
+                    product={product}
+                />
+
+            ))}
+
+        </div>
+
+    );
+}
+
+
+export default ProductGrid;
+
